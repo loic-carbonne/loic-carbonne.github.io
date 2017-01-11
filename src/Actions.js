50fd0903d4f06ca5750eccda5b47761668314597
Loïc Carbonne <loic.carbonne.mail@gmail.com>
2017-01-11 16:14:20 +0100

    Add files via upload

---
+++ src/Actions.js
@@ -0,0 +1,8 @@
+export function SET_LANGUAGE(lang) {
+  return { type: 'SET_LANGUAGE', lang }
+}
+/*
+export function FILTER_PROJECTS(list) {
+  return { type: 'FILTER_PROJECTS', list }
+}
+*/
---
+++ src/Comp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Comp extends Component {
+  render() {
+    return (
+      <p>yoP</p>
+    );
+  }
+}
+
+export default Comp;
