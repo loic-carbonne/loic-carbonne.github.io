5ac24e1c15c1c4f4ca710ced70829622c068db7c
Loïc Carbonne <loicc@theodo.fr>
2017-10-18 22:14:09 +0200

    change personal picture

--- src/Sections/Header.js
+++ src/Sections/Header.js
@@ -4,7 +4,7 @@ let Header = ({strings, lang, onLangChange}) => {
   return (
     <header id="header">
       <div className="inner">
-        <a href="#" className="image avatar"><img src="images/avatar.png" alt="avatar" /></a>
+        <a href="#" className="image avatar"><img src="images/avatar.jpg" alt="avatar" /></a>
         <h1><strong>{strings.header_strongcontent}</strong>{strings.header_content}</h1>
         <div style={{position:"absolute",bottom:15,right:20}}>
         <a className={"flag" + (lang==="en"?" activeFlag":"")} onClick={()=>{onLangChange("en")}}><img alt="English" src="images/english.png" /></a>
