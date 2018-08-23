5d61e737fd6950a18461e17ccfbdd47c8e39aa26
Loïc Carbonne <loicc@theodo.fr>
2018-08-23 18:35:38 +0200

    Make video iframe responsive

--- src/Sections/Marketing.js
+++ src/Sections/Marketing.js
@@ -6,7 +6,9 @@ let AboutMe = ({strings, profile}) => {
       <header className="major">
         <h2>{strings.marketing_title}</h2>
       </header>
-      <iframe src="https://player.vimeo.com/video/279840725" width="640" height="360" frameborder="0" allowfullscreen></iframe>
+      <div className="iframe-container">
+        <iframe className="video-iframe" src="https://player.vimeo.com/video/279840725" frameborder="0" allowfullscreen></iframe>
+      </div>
       <br/>
       <a href={profile.talk_link} target="_blank">{profile.talk_label}</a>
     </section>
